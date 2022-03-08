47df667b746d0553b9a73ba9f40d625f39d9f2fc
小雨 魏 <xiaoyu.wei@shanbay.com>
2022-03-08 18:45:30 +0800

    https://gitee.com/touche-pussy-cat/study_python.git

---
+++ day_01.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+# a=2.9
+# print(a)
+# print(type(a))
+#赋值，数值型，打印数据类型
+
+# a=0
+# b="{}".format(a)
+# print(b)
+# print(type(b))
+#以上四行代码的目的是检验format函数是否用于字符串
+#字符串拼接
+
+# a="ahhahahahah绅士手"
+# b=a.find("ha")
+# print(b)
+#找出字符串ha在a中的位置
+
+# a="迎接另一个晨曦"
+# b=len(a)
+# print(b)
+#求字符串a的长度
+
+a="带来全新空气"
+b=a.replace("带","飞")
+print(b)
+print(a)
+#replace并不会改变原有字符串
+#讲原来字符串中的带改为飞
---
+++ day_02.py
@@ -0,0 +1,25 @@
+# a=("haha","123",'3',(123),345,2,3.4)
+# print(a)
+# b=a[1:3] 
+#实际获取到的数值为尾下标减1
+# print(b)
+# c=a[2]
+# print(c)
+#元祖只读
+
+# a=["123",'123',"""hahah""",78,9.8,["qiantao"]]
+# print(a)
+# b=a[4]
+# print(b);print(type(b))
+# c=a[5]
+# print(c)
+# print(type(c))
+
+a=["hahahahha","远近高低各不同",886]
+a.append("柳暗花明又一村")
+print(a)
+#追加
+
+
+
+
