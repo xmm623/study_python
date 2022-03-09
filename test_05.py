6a45d410daa51ef27a77d90dca75018a3a2eb391
小雨 魏 <xiaoyu.wei@shanbay.com>
2022-03-09 18:46:08 +0800

    test02

--- test_05.py
+++ test_05.py
@@ -9,23 +9,19 @@
 
 from random import choice
 lottery_tickets = (1,2,3,4,5,6,7,8,9,0,'a','B','C','d','e')
-lucky_tickets = []
-while len(lucky_tickets) < 4:
-    pulled_item = choice(lottery_tickets)
-    if pulled_item not in lucky_tickets:
-        lucky_tickets.append(pulled_item)
+
+def get_winning_ticket(self):
+    """摇出中奖彩票"""
+    lucky_tickets = []
+    while len(lucky_tickets) < 4:
+        pulled_item = choice(lottery_tickets)
+        if pulled_item not in lucky_tickets:
+            lucky_tickets.append(pulled_item)
 
 print(lucky_tickets)
-# my_tickets = ()
-# Flag = True
-# while Flag:
-#     my_tickets = sample(lottery_tickets,4)
-#     for lucky in my_tickets:
-#         for really in lucky_tickets:
-#             if lucky == really:
-#                 break
-#             else:
-#                 Flag = False
+my_tickets = []
+
+
 
     
         
