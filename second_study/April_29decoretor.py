8a0975749a7295d99d01f4e1d298ee5dd2c14e98
可爱的小猫咪 <1027818807@qq.com>
2025-04-29 18:20:34 +0800

    装饰器函数初理解

---
+++ second_study/April_29decoretor.py
@@ -0,0 +1,13 @@
+# 装饰器：在不修改原函数代码的前提下，对函数的功能进行扩展。装饰器本质上是一个函数，它接受一个函数作为参数，返回一个新的函数。这个新的函数通常会在原函数的基础上添加一些功能。比如日志记录，性能测试，权限验证等。
+# 装饰器的使用
+def decorator(func): # 装饰器函数
+    def wrapper():
+        print("Before function call")
+        func()
+        print("After function call")
+    return wrapper
+@decorator #装饰器语法糖，等价于 my_function = decorator(my_function)
+def my_function():  # 这是被装饰的函数
+    print("This is the original function.")
+
+my_function()  # 调用被装饰的函数
