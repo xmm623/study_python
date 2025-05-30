06dddd4cfc4807a152cab5698c04677012cfceb6
可爱的小猫咪 <1027818807@qq.com>
2025-05-30 18:33:24 +0800

    自定义包

---
+++ second_study/custom_package/__init__.py
@@ -0,0 +1,2 @@
+# 在这里导入模块中的函数，方便外部使用
+from .math_utils import add, mutiply
---
+++ second_study/custom_package/math_utils.py
@@ -0,0 +1,7 @@
+def add(a,b):
+    """返回两个数的和"""
+    return a + b
+
+def multiply(a, b):
+    """返回两个数的积"""
+    return a * b
