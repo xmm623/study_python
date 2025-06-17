dfca0eb27dd4d177a89c7bcc3f8ecc4fa9b55e4d
可爱的小猫咪 <1027818807@qq.com>
2025-06-17 18:42:08 +0800

    测试执行生命周期与清理机制

---
+++ second_study/june_17set&teardown_test.py
@@ -0,0 +1,64 @@
+# 优先并尽可能地使用 Fixtures。它们是 Pytest 的精髓所在，提供了无与伦比的灵活性、模块化和可复用性。只有在维护一些非常古老的、遵循 xUnit 风格的项目时，或者在编写极其简单的测试脚本时，才考虑使用 xUnit 风格的 setup/teardown 函数。
+# 在 pytest 中，setup 和 teardown 机制用于在测试之前进行必要的准备工作（如初始化环境、准备数据），以及在测试之后清理资源（如关闭连接、删除临时文件）。这种机制确保每个测试用例都在一个干净的环境中运行，避免了测试之间的相互影响。
+# pytest 提供了几种不同的方式来实现 setup 和 teardown 操作，主要包括：fixture函数，类级别的setup/teardown方法，模块级别的setup/teardown方法，但是，最好还是使用fixture，灵活性更高
+# fixture的作用域：
+"""
+scope='function':(默认)每个测试函数都执行一次
+scope='slass':每个测试类执行一次
+scope='module'：每个模块（文件）执行一次
+scope-'session‘:整个测试会话（运行pytest命令）只执行一次
+"""
+import pytest
+
+class Calculator:
+    def add(self,a,b):
+        return a+b
+    
+
+@pytest.fixture(scope='module')
+def module_setup_teardown():
+    """
+    一个模块级的fixture
+    """
+    print("\n---[Fixture-Module]开始执行模块测试---")
+    yield
+    print("\n---[Fixture-Module]模块测试执行完毕---")
+
+@pytest.fixture(scope='function')
+def calculator_instance():
+    """
+    一个函数级的fixture，为每个测试创建一个Calculator实例
+    """
+    print("\n-[Fixture-Function]创建Calculator实例-")
+    calc = Calculator()
+    yield calc # yield将实例提供给测试函数
+    # 每当执行到 yield 语句时，函数会暂停并返回一个值给调用者。当然，返回的这个值可以没有，当再次从生成器请求值时，函数会从上次暂停的地方继续执行，而不是从头开始。
+    # 它是实现 Setup/Teardown 的利器：当 yield 只用一次时，它之前的代码是 Setup，之后的代码是 Teardown，非常适合资源管理，这也是它在 pytest fixtures 和上下文管理器中的核心用法。
+    print("\n-[Fixture-Function]清理Calculator实例-")
+
+def test_addition(calculator_instance,module_setup_teardown):
+    """
+    测试函数讲fixture名称作为参数
+    pytest会自动查找并执行这些fixture
+    """
+    assert calculator_instance.add(2,3) == 5
+
+def test_subtraction(calculator_instance,module_setup_teardown):
+    pass
+
+@pytest.mark.usefixtures("module_setup_teardown")
+class TestCalculatorWithFixtures:
+
+    @pytest.fixture(scope='class')
+    def class_scoped_calculator(self):
+        """
+        一个类级别的fixture
+        """
+        print("\n--[Fixure-Class]为测试类创建一个共享的Calculator实例--")
+        return Calculator()
+    
+    def test_add_positive(self,class_scoped_calculator):
+        assert class_scoped_calculator.add(5,5) == 10
+
+    def test_add_negative(self,class_scoped_calculator):
+        assert class_scoped_calculator.add(-3,-4) == -7
